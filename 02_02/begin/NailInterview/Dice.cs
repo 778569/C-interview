0868fdc729e18fd98e0a15ed89039b7b0255d067
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2024-03-04 16:47:29 +0530

    Add files via upload

---
+++ 02_02/begin/NailInterview/Dice.cs
@@ -0,0 +1,47 @@
+﻿public class Dice
+{
+    static int sidesOfDice = 6;
+    int faceValue = 0;
+
+    public static void changeNumSidesOfDice(int newNumberOfSides)
+    {
+        Dice.sidesOfDice = newNumberOfSides;
+    }
+      
+    public int getFaceValue()
+    {
+        return this.faceValue;
+    }
+      
+    public int roll()
+    {
+        Random rand = new Random();
+        this.faceValue = rand.Next(sidesOfDice) + 1;
+        return this.faceValue;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Creating some dice:
+        // -------------------
+        Console.WriteLine(Dice.sidesOfDice);
+        
+        Dice myFirstDice = new Dice();
+        Dice myOtherDice = new Dice();
+
+       // Rolling two 6-sided dice:
+       // -------------------
+        Console.WriteLine();
+        for (int i = 0; i < 10; i++) {
+            Console.WriteLine("First Die: " + myFirstDice.roll());
+            Console.WriteLine("Second Die: " + myOtherDice.roll());
+        }
+        
+       // Retrieving face value:
+       // -------------------
+        Console.WriteLine();
+        Console.WriteLine("Face Value: " + myFirstDice.getFaceValue());
+        Console.WriteLine("Face Value: " + myOtherDice.getFaceValue());
+
+    }
+}
