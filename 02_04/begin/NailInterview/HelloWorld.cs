e411a0769c83e8159a995d1da411aea523d515c3
Kavinda Bandara <52319671+778569@users.noreply.github.com>
2024-03-04 16:50:36 +0530

    Add files via upload

---
+++ 02_04/begin/NailInterview/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int n = 5;
+        Console.WriteLine("Num outside function: " + n);
+        square(n);
+        Console.WriteLine("Num outside function: " + n);
+    }
+
+    public static void square(int num) {
+        num = num * num;
+        Console.WriteLine("Num inside function: " + num);
+    }
+}
---
+++ 02_04/end/NailInterview/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int n = 5;
+        Console.WriteLine("Num outside function: " + n);
+        square(ref n);
+        Console.WriteLine("Num outside function: " + n);
+    }
+
+    public static void square(ref int num) {
+        num = num * num;
+        Console.WriteLine("Num inside function: " + num);
+    }
+}
